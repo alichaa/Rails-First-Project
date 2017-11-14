87f765499eb42528f9fe0066866b1f76c61f1831
Avraham <lichaa@gmail.com>
2017-11-14 19:13:20 +0000

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   resources :categories
   resources :portfolios
-  get 'pages/home'
+  
+  
 
-  get 'pages/about'
+  get 'about' , to: 'pages#about'
+  get 'contact' , to: 'pages#contact'
+  
 
-  get 'pages/contact'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  
+  #this will take you to the home page by default and you no longer have to do /home in the URL
+  root to: 'pages#home'
+  
 end
